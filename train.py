5a672b1de2eae8f5426bf15badba5530285fd42d
K.Saito <k.saito.1104@gmail.com>
2020-12-23 09:17:55 +0900

    feat: Update instans norm -> batch norm

--- train.py
+++ train.py
@@ -91,7 +91,7 @@ def main(args):
 
     # Create model
     cut = CUT_model(source_shape, target_shape,
-                    cut_mode=args.mode, impl=args.impl, model=args.model)
+                    cut_mode=args.mode, impl=args.impl, model=args.model, norm_layer='batch')
 
     # Define learning rate schedule
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=args.lr,
