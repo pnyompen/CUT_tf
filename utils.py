79a7bfdd1c82968b5982577a35eebebd94849fef
K.Saito <k.saito.1104@gmail.com>
2021-05-29 10:17:21 +0900

    fix

--- utils.py
+++ utils.py
@@ -28,9 +28,10 @@ def load_image(image_file, crop_size, load_size, preprocess='none', data_augment
         image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
     if tar_data_augmentation:
         image = tf.keras.preprocessing.image.random_rotation(
-            image, 25, fill_mode='reflect')
-        zoom_ratio = random.random() * 1 + 0.75 
-        image = tf.keras.preprocessing.image.random_zoom(image, (zoom_ratio, zoom_ratio), fill_mode='reflect')
+            image, 25, fill_mode='reflect', row_axis=0, col_axis=1, channel_axis=2)
+        zoom_ratio = random.random() * 1 + 0.75
+        image = tf.keras.preprocessing.image.random_zoom(
+            image, (zoom_ratio, zoom_ratio), fill_mode='reflect', row_axis=0, col_axis=1, channel_axis=2)
     image = (tf.cast(image, tf.float32) / 127.5) - 1.0
 
     if 'scale_shortside' in preprocess:
